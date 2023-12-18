c9e4c81b1ab0bf14b3a7e3e6b74a2863289cab98
Geek Ricardo <ricardo@GeekdeMac-mini.local>
2023-12-18 11:24:02 +0800

    rich progress with custom changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rprogress",
+    version="0.5.2",
+    description="custom rich progress.",
+    url="https://github.com/GeekRicardo/rprogress",
+    author="Geek·Ricardo",
+    author_email="GeekRicardozzZ@gmail.com",
+    py_modules=["rprogress"],
+    install_requires=["rich"],
+)
